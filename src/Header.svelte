44f9e2c7003ca29052c496d876154baf7e9fd65f
samarthtehri <samarth252002@gmail.com>
2022-03-01 14:22:18 -0500

    V0.0.5 header svelte update

---
+++ src/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {username,user} from './user';
+    function signout() {
+        user.leave();
+        username.set('');
+    }
+</script>
+
+<header>
+    <h1> GUN symbol</h1>
+    {#if $username}
+        <div class="user-bio">
+            <span>Hello <strong>{$username}</strong></span>
+            <img src ={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="image of bear">
+        </div>
+    <button class = "signout-button" on:click={signout}>
+        Sign Out
+    </button>
+    {:else}
+        <h3>Gun.js chat</h3>
+    {/if}
+</header>
